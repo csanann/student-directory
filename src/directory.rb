c742cfb9c245182084ece70d2144d58a9701654b
csanann <csanann@gmail.com>
2023-04-04 12:31:33 +0000

    update more changes

--- src/directory.rb
+++ src/directory.rb
@@ -1,23 +1,32 @@
 #an array that store students information
 students = [
-    "DR. Hannibal Lecter",
-    "Darth Vader",
-    "Nurse Ratched",
-    "Michael Corleone",
-    "Alex Delarge",
-    "The Wicked Witch of the West",
-    "Terminator",
-    "Freddy Krueger",
-    "The Joker",
-    "Joffrey Baratheon",
-    "Norman Bates",
+    {name: "DR. Hannibal Lecter",cohort: :november},
+    {name: "Darth Vader", cohort: :november },
+    {name: "Nurse Ratched", cohort:  :november },
+    {name: "Michael Corleone", cohort: :november },
+    {name: "Alex Delarge", cohort: :november},
+    {name: "The Wicked Witch of the West", cohort: :november },
+    {name: "Terminator", cohort: :november },
+    {name: "Freddy Krueger", cohort: :november },
+    {name: "The Joker", cohort: :november },
+    {name: "Joffrey Baratheon", cohort: :november },
+    {name: "Norman Bates", cohort: :november }
     ]
-#print them out    
-puts "The Students of Villains Academy"
-puts "------------------------------"
-#replace with block of each do
-students.each do |student|
-    puts student
+def print_header
+    puts "The Students of Villains Academy"
+    puts "------------------------------"
 end
-#finally, we print the total number of students
-print "Overall, we have #{student.count} great students"
+
+def print(students)
+    students.each do |student|
+        puts "#{student[:name]} (#{students[:cohort]} cohort)"
+    end
+end
+
+def print_footer(names)
+    puts "Overall, we have #{names.count} great students"
+end
+#call out the methods above
+print_header
+print(students)
+print_footer(students)
