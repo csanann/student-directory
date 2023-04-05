932bef138382fd057ad72bbb6dc12bfb907fe137
csanann <csanann@gmail.com>
2023-04-05 09:04:21 +0000

    update step8 task 4

---
+++ src/directory_step8_4.rb
@@ -0,0 +1,50 @@
+#rewrite the .each() method that prints all st udens using while or until control flow methods (loop)
+
+def input_students
+    puts "Please enter the names of students"
+    puts "To finish, just hit return twice"
+    #create an empty array
+    students = []
+    #get the first name and store to a variable
+    name = gets.chomp
+    #while the name is not empty, repeat the code
+    while !name.empty? do
+        #add the student hash to the array
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        #get another name from the user
+        name =  gets.chomp
+    end
+    #return the array of students
+    students
+end
+
+def print_header
+    puts "The Students of Villains Academy"
+    puts "------------------------------"
+end
+#--------------------------------------------------
+#print method that takes 'letter' parameter to filter the specific names
+def print(students, letter)
+    #set initial index to 0
+    index = 0
+    while index < students.length
+        student = students[index]
+        name = student[:name]
+        #modify a condition in the loop for only name that has characters less than 12
+        if name[0].upcase == letter.upcase && name.length < 12
+            puts "#{index + 1}. #{name } (#{student[:cohort] } cohourt)"
+        end
+        index += 1
+    end
+end
+#---------------------------------------------------^^^^
+def print_footer(students)
+    puts "Overall, we have #{students.count} great students"
+end
+#call out the methods above
+students = input_students
+print_header
+#display out only the students whose names begin with letter A
+print(students, 'A')
+print_footer(students)
